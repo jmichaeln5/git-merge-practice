d79b3e74fb51365c1c23288ff37d90de9832232f
Jon Norton <jmichaeln5@gmail.com>
2018-01-11 11:56:53 -0500

    commiting

--- git-merge-practice/config/routes.rb
+++ git-merge-practice/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'cars#index'
+
+# Whaaaat up Petter!!!!! Boss sauce 2018
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
